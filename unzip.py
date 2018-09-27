858e247543350de3f344c3d2f9e966e6ec0b3477
changzhicheng <879852715@qq.com>
2018-09-27 16:24:10 +0800

    Update unzip.py
    细节处理

--- unzip.py
+++ unzip.py
@@ -22,43 +22,35 @@ from androguard.core import androconf
 from androguard.core.bytecodes import apk
 from androguard.util import read
 
-def run(url): # url is an apk download url
-    list1 = [url]
+def run(url): # url is a apk download url
     try:
-        r1 = requests.head(url,timeout=5)    
+        r1 = requests.head(url,timeout=5)
+        if (r1.status_code == 302):
+            r1 = requests.get(url,stream=True)
         if ('Content-Length' in r1.headers):
             #print (r1.headers)
             range_end = r1.headers['Content-Length']
             range_start = str(int(r1.headers['Content-Length']) - 1024*1024)
-        
+            
             headers = {'Range': 'bytes='+range_start+'-'+range_end}
             r1.close()
-            r = requests.get(url=url,headers=headers,timeout=5) #截取后1M内容
-            #print(r.content)
+            r = requests.get(url=url,headers=headers,timeout=5) #cut the last 1M content and deal 302 status code
             if (b'resources.arsc' in r.content):
                 print("YES")
                 start_46 = r.content.find(b'resources.arsc')
                 start_0 = start_46 - 46
                 if (r.content[start_0:start_0+4]==b'\x50\x4b\x01\x02'):
-                    #print("cunzai")
                     disk_number_start = r.content[start_0+42:start_0+46]
-                    #print(hexdump.dump(disk_number_start))
                     compressed_size = r.content[start_0+20:start_0+24]
                     number_start = int.from_bytes(disk_number_start, byteorder='little')
                     number_end = number_start + int.from_bytes(compressed_size, byteorder='little')
                     if (number_end + 1000 < int(range_end)):
                         headers = {'Range': 'bytes=' + str(number_start) + '-' + str(number_end+1000)}
-                    #zhuan str
-                    #headers = {'Range': 'bytes=0-23744'}
-                    
                     else:
                         headers = {'Range': 'bytes=' + str(number_start) + '-'}
                     r.close()
-                    
-                    #headers = {'Range':'bytes=796249873-'}
                     r3 = requests.get(url=url,headers=headers,timeout=5)
                     a = r3.content
-                    #print(a)
                     r3.close()
                     com_size = a[18:22]
                     name_length = a[26:28]
@@ -68,28 +60,18 @@ def run(url): # url is an apk download url
                     m = int.from_bytes(extra_length, byteorder='little')
                     resc = a[30+n+m:30+n+m+size2]
                     kkk = apk.ARSCParser(resc)
-                    list1.append(kkk.get_packages_names()[0])          
-                    list1.append(kkk.get_string(kkk.get_packages_names()[0],"app_name")[1])
-                    print(list1)
-                    return list1
+                    #list1.append(kkk.get_packages_names()[0])          
+                    x = kkk.get_string(kkk.get_packages_names()[0],"app_name")[1]
+                    print(x)
+                    return x
                 else:
-                    list1.append('')
-                    list1.append('')
-                    return list1
+                    return ('')
             else:
-                list1.append('')
-                list1.append('')
-                return list1
+                return ('')
         else:
-            list1.append('')
-            list1.append('')
-            return list1        
+            return ('')
     except:
-        list1.append('')
-        list1.append('')
-        return list1
-    list1.append('')
-    list1.append('')
-    return list1
+        return ('')
+    return ('')
 	
 # if it has information, it return a list of package_name and app_name
